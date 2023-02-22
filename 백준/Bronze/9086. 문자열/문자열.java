6e6c033b94a123c4d11189d54ab0122a2d2f0bab
bassy <49228024+bassy3260@users.noreply.github.com>
2023-02-22 10:03:45 +0900

    [Bronze V] Title: 문자열, Time: 152 ms, Memory: 16004 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n= Integer.parseInt(br.readLine());
+		for(int i=0;i<n;i++) {
+			String str=br.readLine();
+			
+			bw.write(String.valueOf(str.charAt(0))+String.valueOf(str.charAt(str.length()-1))+"\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
