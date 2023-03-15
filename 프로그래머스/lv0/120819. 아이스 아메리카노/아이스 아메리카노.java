9d0a1374594f5dff4a1eda09fdb28da08bfa23a6
bassy <49228024+bassy3260@users.noreply.github.com>
2023-03-15 16:04:34 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {0, 0};
+        
+        while (money / 5500 > 0) {
+            answer[0]++;
+            money -= 5500;
+        }
+        answer[1] = money%5500;
+        
+        return answer;
+    }
+}
