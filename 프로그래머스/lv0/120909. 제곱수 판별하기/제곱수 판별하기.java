216c9c3fb0fc711ea1b129af5002a63478bb6d88
bassy <49228024+bassy3260@users.noreply.github.com>
2023-03-15 19:04:56 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (n%i == 0) {
+                if (n/i == i) {
+                answer = 1;
+                }
+            }
+        }
+            
+        if (answer == 0) {
+            answer = 2;
+        }
+        
+        return answer;
+    }
+}
