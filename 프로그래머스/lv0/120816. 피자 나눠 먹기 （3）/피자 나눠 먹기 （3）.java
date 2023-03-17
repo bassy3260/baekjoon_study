39aaf845a094afad489eca195c2619ad3bfec138
bassy <49228024+bassy3260@users.noreply.github.com>
2023-03-17 09:19:01 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int slice, int n) {
-        int answer = 0; // 판 개수
-        answer += slice; // 1판 조각 더하고 시작
+        int answer = 0;
         
-        while(answer < n) {
-            answer += slice; // 1판 조각 더 더함
+        if((n%slice !=0)){
+            answer=n/slice+1;
+        }else{
+            answer=n/slice;
         }
-        
-        answer = answer / slice; // 조각 int로 나누면 몫만 깔끔하게 남음
-        
         return answer;
     }
 }
