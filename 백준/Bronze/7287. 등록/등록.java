65f1b7bdcf94bdb0e645e98f8a06a8ba5564bb1c
bassy <49228024+bassy3260@users.noreply.github.com>
2025-12-02 22:47:54 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		System.out.println(351);
+		System.out.println("bassy3260");
+	}
+}
