d5dd55622af3f5489cb3dab8b007a0e42ef197ff
bassy <49228024+bassy3260@users.noreply.github.com>
2025-12-03 23:24:46 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+
+import java.io.IOException;
+
+import java.math.BigInteger; // BigInteger 클래스 임포트
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        
+
+        System.out.println("A");       // a + b
+
+
+    }
+
+}
